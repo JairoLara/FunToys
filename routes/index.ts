4838d277e9607034915394c04fbd3b6e3359cecb
JairoLara <laracisnerosjairo@gmail.com>
2025-02-21 00:35:04 -0600

    funcion

--- routes/index.ts
+++ routes/index.ts
@@ -25,6 +25,53 @@ router.get('/usuarios', async (req, res) => {
   }
 });
 
+
+router.post('/login', async (req, res) => {
+  const { email, contraseña } = req.body;
+  
+  try {
+    const usuario = await Usuario.findOne({ where: { email } });
+    
+    if (!usuario) {
+      return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
+    }
+    
+    if (usuario.contraseña !== contraseña) {
+      return res.status(400).json({ success: false, message: 'Contraseña incorrecta' });
+    }
+    
+    return res.status(200).json({ success: true, message: 'Login exitoso' });
+  } catch (error) {
+    console.error('Error en el servidor:', error);
+    return res.status(500).json({ success: false, message: 'Error en el servidor', error: error.message });
+  }
+});
+
+//registro
+router.post('/register', async (req, res) => {
+  const { nombre, email, contraseña, rol } = req.body;
+  
+  try {
+    const usuarioExistente = await Usuario.findOne({ where: { email } });
+    
+    if (usuarioExistente) {
+      return res.status(400).json({ success: false, message: 'El correo electrónico ya está registrado' });
+    }
+    
+    const nuevoUsuario = await Usuario.create({
+      nombre,
+      email,
+      contraseña,
+      rol
+    });
+    
+    return res.status(201).json({ success: true, message: 'Usuario registrado exitosamente', usuario: nuevoUsuario });
+  } catch (error) {
+    console.error('Error al registrar el usuario:', error);
+    return res.status(500).json({ success: false, message: 'Error en el servidor', error: error.message });
+  }
+});
+
 router.get('/productos', async (req, res) => {
   try {
     const usuarios = await Usuario.findAll();
@@ -41,68 +88,21 @@ router.get('/productos', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
-  const { email, contraseña } = req.body;
-
-  try {
-    const usuario = await Usuario.findOne({ where: { email } });
-
-    if (!usuario) {
-      return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
-    }
-
-    if (usuario.contraseña !== contraseña) {
-      return res.status(400).json({ success: false, message: 'Contraseña incorrecta' });
-    }
-
-    return res.status(200).json({ success: true, message: 'Login exitoso' });
-  } catch (error) {
-    console.error('Error en el servidor:', error);
-    return res.status(500).json({ success: false, message: 'Error en el servidor', error: error.message });
-  }
-});
-
-//registro
-router.post('/register', async (req, res) => {
-  const { nombre, email, contraseña, rol } = req.body;
-
-  try {
-    const usuarioExistente = await Usuario.findOne({ where: { email } });
-
-    if (usuarioExistente) {
-      return res.status(400).json({ success: false, message: 'El correo electrónico ya está registrado' });
-    }
-
-    const nuevoUsuario = await Usuario.create({
-      nombre,
-      email,
-      contraseña,
-      rol
-    });
-
-    return res.status(201).json({ success: true, message: 'Usuario registrado exitosamente', usuario: nuevoUsuario });
-  } catch (error) {
-    console.error('Error al registrar el usuario:', error);
-    return res.status(500).json({ success: false, message: 'Error en el servidor', error: error.message });
-  }
-});
-
-
 // Ruta para actualizar productos
 router.put("/products/:id", async (req, res) => {
   const productId = req.params.id;
   const { name, description, price, discount, stock, image } = req.body;
-
+  
   console.log("ID recibido:", productId); // Para verificar el ID
   console.log("Datos recibidos:", req.body); // Para verificar los datos
-
+  
   try {
     const product = await Product.findByPk(productId);
-
+    
     if (!product) {
       return res.status(404).json({ message: "Producto no encontrado" });
     }
-
+    
     const updatedProduct = await product.update({
       name,
       description,
@@ -111,7 +111,7 @@ router.put("/products/:id", async (req, res) => {
       stock,
       image,
     });
-
+    
     res.json(updatedProduct);
   } catch (error) {
     console.error(error);
@@ -122,7 +122,7 @@ router.put("/products/:id", async (req, res) => {
 // Ruta para eliminar productos
 router.delete("/products/:id", async (req, res) => {
   const productId = req.params.id;
-
+  
   try {
     // Buscar el producto por ID
     const product = await Product.findByPk(productId);
