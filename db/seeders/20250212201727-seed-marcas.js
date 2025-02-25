9e62ace23981310548d272ee142f2346a6e07446
MiKey828282 <mikeyperez828@gmail.com>
2025-02-24 20:56:43 -0600

    registro de juguetes y marcas

--- db/seeders/20250212201727-seed-marcas.js
+++ db/seeders/20250212201727-seed-marcas.js
@@ -4,7 +4,14 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('marcas', [
       { nombre: 'Lego', createdAt: new Date(), updatedAt: new Date() },
-      { nombre: 'Mattel', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'Hotwheels', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'Megablock', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'Barbie', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'UNO', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'Transformers', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'Monster High', createdAt: new Date(), updatedAt: new Date() },
+      { nombre: 'Roblox', createdAt: new Date(), updatedAt: new Date() }
+      
     ]);
   },
 
